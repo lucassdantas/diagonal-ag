d38162e639d91b187897d3931f89d19f947da842
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-02-15 15:22:00 -0300

    configure color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        purple_100:'#2D0C50',
+        purple_90:'#441772',
+        purple_80:'#5C2694',
+        purple_70:'#7638B6',
+        purple_60:'#924ED8',
+        purple_50:'#B068FA',
+        purple_40:'#C48BFF',
+        purple_30:'#D5ADFF',
+        purple_20:'#E6CEFF',
+        purple_10:'#F7F0FF',
+        main_black:'#1E1E1E',
+        gray_10:'#2C2C2C'
       },
     },
   },
