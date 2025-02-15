fb7a3b3819eeb2ba9f755d9f43edb94d60bc1588
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-02-15 15:18:23 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
+import { Banner } from "@/app/components/Banner";
+import { Footer } from "@/app/components/Footer";
+import { Header } from "@/app/components/Header";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      
+    <>
+      <Header/>
+      <main className="">
+        <Banner/>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        
-        </a>
-      </footer>
-    </div>
+      <Footer/>
+    </>
   );
 }
