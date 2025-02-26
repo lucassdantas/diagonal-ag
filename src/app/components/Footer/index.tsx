50ca1e06e7183d2bd444bcc441ca2b7715cda9a3
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-02-26 00:59:09 -0300

    style: building footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -7,15 +7,17 @@ import { Title } from '@/app/components/Title'
 import { BiEnvelope } from 'react-icons/bi'
 export const Footer = () => {
   return (
-    <footer className='flex flex-col gap-4 bg-main_black text-main_white p-4 text-center '>
-      <p>Pense <span className='text-purple_60'>diferente</span>. Pense na <span className='text-purple_60'>Diagonal</span>.</p>
-      <Image src={diagonalLogoWithoutBg} alt='Logo diagonal vazada'/>
-      <Title tag='h6' className='font-normal mt-4'>Confira nossas redes sociais:</Title>
-      <div className='flex gap-4 justify-center'>
-        {allSocialNetworksList.map((socialNetowrk, i) => ( <Link href={socialNetowrk.url} target='_blank' key={i}><socialNetowrk.icon className='text-purple_40 text-3xl'/></Link>))}
+    <footer className=' bg-main_black text-main_white pt-4 text-center '>
+      <div className='flex flex-col gap-4 px-4 mb-8'>
+        <p>Pense <span className='text-purple_60'>diferente</span>. Pense na <span className='text-purple_60'>Diagonal</span>.</p>
+        <Image src={diagonalLogoWithoutBg} alt='Logo diagonal vazada'/>
+        <Title tag='h6' className='font-normal mt-4'>Confira nossas redes sociais:</Title>
+        <div className='flex gap-4 justify-center'>
+          {allSocialNetworksList.map((socialNetowrk, i) => ( <Link href={socialNetowrk.url} target='_blank' key={i}><socialNetowrk.icon className='text-purple_40 text-3xl'/></Link>))}
+        </div>
+        <span className='flex justify-center items-center gap-4'><BiEnvelope className='text-purple_40 text-3xl'/> contato@diagonal.ag</span>
       </div>
-      <span className='flex justify-center items-center gap-4'><BiEnvelope className='text-purple_40 text-3xl'/> contato@diagonal.ag</span>
-      <div className='bg-gray_10 text-white text-center'>
+      <div className='bg-gray_10 text-white text-center p-4'>
         <small>© {new Date().getFullYear()} Diagonal - Todos os direitos reservados</small>
       </div>
     </footer>
