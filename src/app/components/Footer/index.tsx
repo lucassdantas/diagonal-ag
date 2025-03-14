73e20428cd91b437c8e1c3385bd097014eb2e72c
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-03-14 20:15:11 -0300

    add clikcup logo to footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -5,14 +5,21 @@ import { Title } from '@/app/components/Title'
 import { BiEnvelope } from 'react-icons/bi'
 export const Footer = () => {
   return (
-    <footer className=' bg-main_black text-main_white pt-8 text-center '>
+    <footer className='bg-main_black text-main_white pt-8 text-center '>
       <div className='flex flex-col items-center gap-4 px-4 mb-12'>
-        <p>Pense <span className='text-purple_60'>diferente</span>. Pense na <span className='text-purple_60'>Diagonal</span>.</p>
+        <p className='font-bold'>Pense <span className='text-purple_50'>diferente</span>. Pense na <span className='text-purple_50'>Diagonal</span>.</p>
         <img src={'/assets/diagonal-logo-vazada.png'} alt='Logo diagonal vazada'/>
-        <img className='max-w-[73px] lg:max-w-[172px]' src={'/assets/footer/clickup-verified-logo.png'} alt='clickup verified logo'/>
+        <img className='max-w-[73px] lg:hidden' src={'/assets/footer/clickup-verified-logo.png'} alt='clickup verified logo'/>
         <Title tag='h6' className='font-normal mt-4'>Confira nossas redes sociais:</Title>
-        <div className='flex gap-4 justify-center'>
-          {allSocialNetworksList.map((socialNetowrk:any, i) => ( <Link href={socialNetowrk.url} target='_blank' key={i}><socialNetowrk.icon className='text-purple_40 text-3xl'/></Link>))}
+        <div className='flex items-center w-full'>
+          <div className='w-1/3'>
+          </div>
+          <div className='flex gap-4 justify-center w-1/3'>
+            {allSocialNetworksList.map((socialNetowrk:any, i) => ( <Link href={socialNetowrk.url} target='_blank' key={i}><socialNetowrk.icon className='text-purple_40 text-3xl'/></Link>))}
+          </div>
+          <div className='w-1/3 flex justify-center absolute right-0'>
+            <img className='max-w-[172px] text-center' src={'/assets/footer/clickup-verified-logo.png'} alt='clickup verified logo'/>
+          </div>
         </div>
         <span className='flex justify-center items-center gap-4'><BiEnvelope className='text-purple_40 text-3xl'/> contato@diagonal.ag</span>
       </div>
